a4707c18b97c8682715e8d5558c0ac8ef8fbc1dd
FelixJentsch <FelixJentsch91@googlemail.com>
2023-12-15 14:52:13 +0100

    blub

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,13 @@
+import useSWR from "swr";
+
+const fetcher = (url) => fetch(url).then((res) => res.json());
+
+function ArtPieces() {
+  const { data } = useSWR("https://example-apis.vercel.app/api/art", fetcher);
+}
+
+return {};
+
 export default function HomePage() {
   return (
     <div>
