3a0a3107a7b13a5965bda08eb8d442b8a005815d
Sarah Morgen <sarah.morgen@outlook.de>
2023-12-18 00:38:23 +0100

    New Components Artpieces/Spotlight

--- pages/components/spotlight.js
+++ pages/components/spotlight.js
@@ -1,16 +1,16 @@
-import GlobalStyle from "../styles";
-import { SWRConfig } from "swr";
 import React from 'react';
+import Image from "next/image";
 
 
-const spotlight =({image, artist}) => (                         //Spotlight erstellt mit den zwei Probs rendert Image und artist
-    <div>                 
-    <img src={image} alt="spotlight" />
-        <p>Artist {artist}</p>                                 {/*interpolation damit der Artist wechseln kann*/}
+const Spotlight = ({ image, artist }) => (                         //NEU Spotlight erstellt mit den zwei Probs rendert Image und artist
+    <div>
+        <Image src={image} width={700} height={700} />          {/* Hier wird das SpotlightBild übergeben und mit einer größe definiert*/}
+        <p>Artist {artist}</p>                                 {/*NEU interpolation damit der Artist wechseln kann*/}
     </div>
 );
 
 
 
 
-export default spotlight;
+
+export default Spotlight;
--- pages/index.js
+++ pages/index.js
@@ -1,40 +1,16 @@
 import useSWR from "swr";
-import { useRouter } from "next/router";
-import Link from "next/link";
-import Image from "next/image";
+import ArtPieces from "./components/ArtPieces";
 
-export default function ArtPieces() {
+export default function index (){
+
+
   const { data, error } = useSWR("https://example-apis.vercel.app/api/art");
-
-  function ArtPiecePreview({ name, slug, imageSource, artist }) {
-    return (
-      <div>
-        <Link href={`/art-pieces/${slug}`}>
-          <ul>
-            <Image src={imageSource} alt={name} width={400} height={300} />
-            <li>{name}</li>
-            <li>{artist}</li>
-          </ul>
-        </Link>
-      </div>
-    );
-  }
-
-  const pieces = data;
-  if (error) return <div>Error loading data</div>;
-  if (!data) return <div>Loading...</div>;
-
-  return (
-    <div>
-      {pieces.map((piece) => (
-        <ArtPiecePreview
-          key={piece.slug}
-          slug={piece.slug}
-          imageSource={piece.imageSource}
-          name={piece.name}
-          artist={piece.artist}
-        />
-      ))}
-    </div>
-  );
+  const pieces = data;                                          //NEU ist nun hier
+    if (error) return <div>Error loading data</div>;
+    if (!data) return <div>Loading...</div>;
+ return (
+  <div>
+   <ArtPieces pieces={pieces} ></ArtPieces>                  {/*NEU hier wird nun pieces an ArtPieces übergeben*/}
+  </div>
+ )
 }
